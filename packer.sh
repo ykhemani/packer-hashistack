7f62f21cda3e95197f9af53c946eb1ec5b598b8e
Yash Khemani <khemani@m13.home.seva.cafe>
2021-04-04 23:23:07 +0000

    use variables file

--- packer.sh
+++ packer.sh
@@ -2,12 +2,12 @@
 
 # build unencrypted ami
 packer build \
-  -var "owner=$(cat var.owner)" \
+  -var-file="yash.pkrvars.hcl" \
   .
 
 # build encrypted ami
-packer build \
-  -var "owner=$(cat var.owner)" \
-  -var "encrypt_boot=true" \
-  -var "aws_kms_key_id=$(cat var.aws_kms_key_id)" \
-  .
+#packer build \
+#  -var "owner=$(cat var.owner)" \
+#  -var "encrypt_boot=true" \
+#  -var "aws_kms_key_id=$(cat var.aws_kms_key_id)" \
+#  .
